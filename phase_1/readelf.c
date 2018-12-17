ba9807f0e465073f398d6a6c2b3d63dd927720bc
Hydriss <hydrissruet@gmail.com>
2018-12-17 15:29:54 +0100

    etape 1 oui

--- phase_1/readelf.c
+++ phase_1/readelf.c
@@ -13,7 +13,7 @@ int main(){
 	}
 	while (!feof(ptr)){
 		c = fgetc(ptr);
-		if (c == '\0' && i>10){
+		if (c == '\0' && i>200){
 			break;
 		}
 		printf("0x%x ",c);
@@ -55,7 +55,136 @@ int main(){
 	printf("\n");
 	printf("version : %d",tab[6]);
 	printf("\n");
-	printf("7 : 0x%x",tab[7]);
+	printf("ABI : ");
+	switch (tab[7]) {
+		case 0:
+			printf("UNIX System V");
+			break;
+		case 1:
+			printf("HP-UX");
+			break;
+		case 2:
+			printf("NetBSD");
+			break;
+		case 3:
+			printf("Linux");
+			break;
+		case 6:
+			printf("Sun Solaris");
+			break;
+		case 7:
+			printf("IBM AIX");
+			break;
+		case 8:
+			printf("SGI Irix");
+			break;
+		case 9:
+			printf("FreeBSD");
+			break;
+		case 10:
+			printf("Compaq TRU64");
+			break;
+		case 11:
+			printf("Novell Modesto");
+			break;
+		case 12:
+			printf("OpenBSD");
+			break;
+		case 64:
+			printf("ARM EABI");
+			break;
+		case 97:
+			printf("ARM");
+			break;
+		case 255:
+			printf("Standalone");
+			break;
+		default:
+			printf("???");
+
+	}
+	printf("\n");
+	printf("Version ABI : 0x%x\n",tab[8]);
+	printf("type : ");
+	switch (tab[16]) {
+		case 0:
+			printf("not file type");
+			break;
+		case 1:
+			printf("relocatable file");
+			break;
+		case 2:
+			printf("executable file");
+			break;
+		case 3:
+			printf("shared object file");
+			break;
+		case 4:
+			printf("corefile");
+			break;
+		case 0xff00:
+			printf("Processor-scpecifique");
+			break;
+		case 0xffff:
+			printf("Processor-scpecifique");
+			break;
+		default :
+			printf("???");
+	}
+	printf("\n");
+	printf("type machine: ");
+	switch (tab[18]) {
+		case 0:
+			printf("aucune");
+			break;
+		case 2:
+			printf("SPARC");
+			break;
+		case 3:
+			printf("intel 80386");
+			break;
+		case 4:
+			printf("Motorola 68000");
+			break;
+		case 7:
+			printf("Intel i860");
+			break;
+		case 8:
+			printf("MIPS I");
+			break;
+		case 19:
+			printf("Intel i960");
+			break;
+		case 20:
+			printf("PowerPC");
+			break;
+		case 40:
+			printf("ARM");
+			break;
+		case 50:
+			printf("Intel IA64");
+			break;
+		case 62:
+			printf("x64");
+			break;
+		default :
+			printf("???");
+	}
+	printf("\n");
+	printf("version : %s",(!tab[20])? "Aucune" : "Actuelle");
+	printf("\n");
+	printf("adresse d'entree : 0x%x\n",((tab[24] << 0) + (tab[25] << 8) + (tab[26] << 16) + (tab[27] << 24)));
+	int offsetprg = (tab[28] << 0) + (tab[29] << 8) + (tab[30] << 16) + (tab[31] << 24);
+	printf("offset des entetes de programme : %d\n",offsetprg);
+	int offsetsec = (tab[32] << 0) + (tab[33] << 8) + (tab[34] << 16) + (tab[35] << 24);
+	printf("offset des entetes de sections : %d\n",offsetsec);
+	printf("fanions : 0x%x\n",((tab[36] << 0) + (tab[37] << 8) + (tab[38] << 16) + (tab[39] << 24)));
+	printf("taille de cette entete : %d\n",((tab[40] << 0) + (tab[41] << 8)));
+	printf("taille de l'entete du programme : %d\n",((tab[42] << 0) + (tab[43] << 8)));
+	printf("nombre d'entete du programme : %d\n",((tab[44] << 0) + (tab[45] << 8)));
+	printf("taille des entetes de sections : %d\n",((tab[46] << 0) + (tab[47] << 8)));
+	printf("nombres d'entetes de sections : %d\n",((tab[48] << 0) + (tab[49] << 8)));
+	printf("table d'index des chaines d'entete de sections : %d\n",((tab[50] << 0) + (tab[51] << 8)));
 
 	return 0;
 }
