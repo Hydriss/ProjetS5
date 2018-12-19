2ca367df91c086eb50b19d4bd2de7c6c62b25788
islame ladly <islameladly@outlook.com>
2018-12-19 11:14:03 +0100

    oui

--- phase_1/readelf.c
+++ phase_1/readelf.c
@@ -2,36 +2,190 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
-	int i,j;
-	char strH[120];
+int main(int argc, char* argv[]){
 	char c;
+	int i = 0;
+	int tab[200];
 	FILE *ptr = malloc(sizeof(FILE));
-	ptr = fopen("/home/s/salamanl/projet/elf_linker-1.0/Examples_loader/example1.o","r");
+	ptr = fopen(argv[1],"r");
 	if (ptr==NULL){
 		printf("Erreur fopen");
 	}
 	while (!feof(ptr)){
 		c = fgetc(ptr);
-		if (c == '\0'){
+		if (c == '\0' && i>200){
 			break;
 		}
 		printf("0x%x ",c);
+		tab[i] = c;
+		i++;
 	}
-	/*char l[120];
-	if(fgets(l,120,ptr)!= NULL){
-			/*set strH with nulls
-	    memset(strH,0,sizeof(strH));
+	printf("identification : 0x%x",tab[0]);
+	for(i = 1; i <=3;i++){
+		printf("%c",tab[i]);
+	}
+	printf("\nclass : ");
+	switch (tab[4]) {
+		case 0:
+			printf("ClassNONE");
+			break;
+		case 1:
+			printf("Class32");
+			break;
+		case 2:
+			printf("Class64");
+			break;
+		default :
+			printf("invalid");
+	}
+	printf("\ndata : ");
+	switch (tab[5]) {
+		case 0:
+			printf("dataNONE");
+			break;
+		case 1:
+			printf("dataLSB");
+			break;
+		case 2:
+			printf("dataMSB");
+			break;
+		default :
+			printf("invalid");
+	}
+	printf("\n");
+	printf("version : %d",tab[6]);
+	printf("\n");
+	printf("ABI : ");
+	switch (tab[7]) {
+		case 0:
+			printf("UNIX System V");
+			break;
+		case 1:
+			printf("HP-UX");
+			break;
+		case 2:
+			printf("NetBSD");
+			break;
+		case 3:
+			printf("Linux");
+			break;
+		case 6:
+			printf("Sun Solaris");
+			break;
+		case 7:
+			printf("IBM AIX");
+			break;
+		case 8:
+			printf("SGI Irix");
+			break;
+		case 9:
+			printf("FreeBSD");
+			break;
+		case 10:
+			printf("Compaq TRU64");
+			break;
+		case 11:
+			printf("Novell Modesto");
+			break;
+		case 12:
+			printf("OpenBSD");
+			break;
+		case 64:
+			printf("ARM EABI");
+			break;
+		case 97:
+			printf("ARM");
+			break;
+		case 255:
+			printf("Standalone");
+			break;
+		default:
+			printf("???");
 
-	    /*converting str character into Hex and adding into strH
-	    for(i=0,j=0;i<strlen(l);i++,j+=2)
-	    {
-	        sprintf((char*)strH+j,"%02X",l[i]);
-	    }
-	    strH[j]='\0'; /*adding NULL in the end
-		printf("ELF Header \n");
-	    printf("Magic : %s\n",strH);
-		printf("%s \n",l);
-	}*/
-	fclose(ptr);
+	}
+	printf("\n");
+	printf("Version ABI : 0x%x\n",tab[8]);
+	printf("type : ");
+	switch (tab[16]) {
+		case 0:
+			printf("not file type");
+			break;
+		case 1:
+			printf("relocatable file");
+			break;
+		case 2:
+			printf("executable file");
+			break;
+		case 3:
+			printf("shared object file");
+			break;
+		case 4:
+			printf("corefile");
+			break;
+		case 0xff00:
+			printf("Processor-scpecifique");
+			break;
+		case 0xffff:
+			printf("Processor-scpecifique");
+			break;
+		default :
+			printf("???");
+	}
+	printf("\n");
+	printf("type machine: ");
+	switch (tab[18]) {
+		case 0:
+			printf("aucune");
+			break;
+		case 2:
+			printf("SPARC");
+			break;
+		case 3:
+			printf("intel 80386");
+			break;
+		case 4:
+			printf("Motorola 68000");
+			break;
+		case 7:
+			printf("Intel i860");
+			break;
+		case 8:
+			printf("MIPS I");
+			break;
+		case 19:
+			printf("Intel i960");
+			break;
+		case 20:
+			printf("PowerPC");
+			break;
+		case 40:
+			printf("ARM");
+			break;
+		case 50:
+			printf("Intel IA64");
+			break;
+		case 62:
+			printf("x64");
+			break;
+		default :
+			printf("???");
+	}
+	printf("\n");
+	printf("version : %s",(!tab[20])? "Aucune" : "Actuelle");
+	printf("\n");
+	printf("adresse d'entree : 0x%x\n",((tab[24] << 0) + (tab[25] << 8) + (tab[26] << 16) + (tab[27] << 24)));
+	int offsetprg = (tab[28] << 0) + (tab[29] << 8) + (tab[30] << 16) + (tab[31] << 24);
+	printf("offset des entetes de programme : %d\n",offsetprg);
+	int offsetsec = (tab[32] << 0) + (tab[33] << 8) + (tab[34] << 16) + (tab[35] << 24);
+	printf("offset des entetes de sections : %d\n",offsetsec);
+	printf("fanions : 0x%x\n",((tab[36] << 0) + (tab[37] << 8) + (tab[38] << 16) + (tab[39] << 24)));
+	printf("taille de cette entete : %d\n",((tab[40] << 0) + (tab[41] << 8)));
+	printf("taille de l'entete du programme : %d\n",((tab[42] << 0) + (tab[43] << 8)));
+	printf("nombre d'entete du programme : %d\n",((tab[44] << 0) + (tab[45] << 8)));
+	printf("taille des entetes de sections : %d\n",((tab[46] << 0) + (tab[47] << 8)));
+	printf("nombres d'entetes de sections : %d\n",((tab[48] << 0) + (tab[49] << 8)));
+	printf("table d'index des chaines d'entete de sections : %d\n",((tab[50] << 0) + (tab[51] << 8)));
+
+	return 0;
 }
+
