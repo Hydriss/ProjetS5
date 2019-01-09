67a875e5f8fd6d0e6345c8d31731af124e75ad77
Hydriss <hydrissruet@gmail.com>
2019-01-09 10:13:35 +0100

    recup

--- phase_2/main.c
+++ phase_2/main.c
@@ -10,5 +10,10 @@ int findSymbole(char* nom, symb* symbList, int tailleSymbList){
 }
 
 int main(int argc, char const *argv[]) {
+  int tab[10] = {0,1,2,3,4,5,6,7,8,9};
+  int taille = decoupeTab(tab,10,5,2);
+  for(int i = 0; i < taille;i++){
+    print("%d\n",tab[i]);
+  }
   return 0;
 }
